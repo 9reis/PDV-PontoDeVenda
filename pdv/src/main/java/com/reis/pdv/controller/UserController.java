c12209263ed70648c09d177b7113a2bf62a6ba9f
Lucas Reis <lucasreis_9@hotmail.com>
2022-10-31 11:37:54 -0300

    Editanto usuario

--- pdv/src/main/java/com/reis/pdv/controller/UserController.java
+++ pdv/src/main/java/com/reis/pdv/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.reis.pdv.controller;
 
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,6 +34,7 @@ public class UserController {
 	public ResponseEntity post(@RequestBody User user) {
 		
 		try {
+			user.setEnabled(true);
 			return new ResponseEntity<>(userRepository.save(user),HttpStatus.CREATED);
 		}catch(Exception error) {
 			return new ResponseEntity<>(error.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
@@ -38,5 +42,16 @@ public class UserController {
 		
 	}
 	
+	@PutMapping()
+	public ResponseEntity put( @RequestBody User user) {
+		Optional<User> userToEdit = userRepository.findById(user.getId());
+	
+		if(userToEdit.isPresent()) {
+			userRepository.save(user);
+			return new ResponseEntity<>(user, HttpStatus.OK);
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
 	
 }
