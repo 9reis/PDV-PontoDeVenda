b5b4a9c5ff2c6fa7d7f6eb519f251d2eabd2cba1
Lucas Reis <lucasreis_9@hotmail.com>
2022-10-31 12:01:15 -0300

    Deletando usuarios

--- pdv/src/main/java/com/reis/pdv/controller/UserController.java
+++ pdv/src/main/java/com/reis/pdv/controller/UserController.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -52,6 +54,18 @@ public class UserController {
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity delete(@PathVariable Long id) {
+		
+		try {
+			userRepository.deleteById(id);
+			return new ResponseEntity<>("Usuário removido com sucesso!",HttpStatus.OK);
+		}catch(Exception error) {
+			return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+			
+		}
 	}
 	
 }
